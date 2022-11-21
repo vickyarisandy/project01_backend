53ab4d61623c1eb87762a9840c816d9f3aa91918
VA <54583234+vickyarisandy@users.noreply.github.com>
2022-11-21 09:01:54 +0800

    update function on tips controller

--- app/Http/Controllers/TipsController.php
+++ app/Http/Controllers/TipsController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tip;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 
 class TipsController extends Controller
 {
@@ -13,17 +16,10 @@ class TipsController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $tips =Tip::paginate(5);
+        return response()->json([
+            'data'=> $tips,
+        ]);
     }
 
     /**
@@ -34,7 +30,14 @@ class TipsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $tips = Tip::create([
+            'title' => $request->title,
+            'thumbnail' => $request->thumbnail,
+            'url' => $request->url,
+        ]);
+        return response()->json([
+            'data' => $tips,
+        ]);
     }
 
     /**
@@ -45,18 +48,15 @@ class TipsController extends Controller
      */
     public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        // return response()->json([
+        //     'data'=> Tip::where('id',$id)->get(),
+        //     'token' => null,
+        // ]);
+        $program = Tip::find($id);
+        if (is_null($program)) {
+            return response()->json(['message'=>'Data not found'], 404); 
+        }
+        return response()->json(['message'=>'OK','data'=>$program]);
     }
 
     /**
@@ -68,7 +68,30 @@ class TipsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // $tips->title = $request->title;
+        // $tips->thumbnail = $request->thumbnail;
+        // $tips->url = $request->url;
+        // $tips->update();
+        // return response()->json([
+        //     'data' => $tips,
+        // ]);
+        $validator = Validator::make($request->all(),[
+            'title' => 'required|string|max:255',
+            'thumbnail' => 'required',
+            'url' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors());       
+        }
+
+        $tips->title = $request->title;
+        $tips->thumbnail = $request->thumbnail;
+        $tips->url = $request->url;
+        $tips->save();
+        
+        return response()->json(['message'=>'Program updated successfully.']);
+
     }
 
     /**
@@ -79,6 +102,19 @@ class TipsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        // Tip::delete('id',$id);
+        $tips = Tip::find($id);
+        if ($tips) {
+            $tips->delete();
+            return response()->json([
+            'message' => 'terhapus ...',
+        ]);
+        } else {
+            return response()->json([
+            'message' => 'cek data ...',
+        ]);
+        }
+        
+        
     }
 }
