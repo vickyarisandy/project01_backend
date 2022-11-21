10ffba0514d9c72ccbc0a086062b7389f50d736b
VA <54583234+vickyarisandy@users.noreply.github.com>
2022-11-21 17:07:02 +0800

    seeder admin_user, payment_methode

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admin_users')->insert([
+            'name' => 'user1',
+            'email' => 'user1@wallet.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
